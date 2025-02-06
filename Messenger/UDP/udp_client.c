672d6c490d0a4b73c6d4252aefb2bf3c75a889ae
r4ff1n <r4ff1n@proton.me>
2025-02-06 20:30:09 +0900

    2025.02.06

--- Messenger/UDP/udp_client.c
+++ Messenger/UDP/udp_client.c
@@ -1,25 +1,59 @@
-// 소켓 생성, 연결, 데이터 송수신, 종료
-// socket(), connect(), send(), recv(), close() 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SERVER_IP "127.0.0.1"  // 서버 IP (로컬 서버)
+#define SERVER_PORT 9000      // 서버 포트
+#define BUF_SIZE 1024
 
 int main() {
     int sock;
+    struct sockaddr_in server_addr;
+    char buffer[BUF_SIZE];
 
-    // 1. 소켓 생성
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+    // 1. UDP 소켓 생성
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock == -1) {
         perror("소켓 생성 실패");
         exit(1);
     }
-    printf("소켓 생성 성공!\n");
 
-    // 2. 소켓 닫기
+    // 2. 서버 주소 구조체 설정
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+        perror("서버 IP 주소 변환 실패");
+        exit(1);
+    }
+
+    // 3. 사용자로부터 메시지 입력 받기
+    printf("서버로 보낼 메시지를 입력하세요: ");
+    fgets(buffer, BUF_SIZE, stdin);
+    buffer[strcspn(buffer, "\n")] = 0;  // 입력된 문자열에서 개행 문자 제거
+
+    // 4. 서버로 메시지 전송
+    if (sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("서버로 메시지 전송 실패");
+        close(sock);
+        exit(1);
+    }
+
+    // 5. 서버로부터 응답 수신
+    socklen_t addr_len = sizeof(server_addr);
+    int recv_len = recvfrom(sock, buffer, BUF_SIZE, 0, (struct sockaddr*)&server_addr, &addr_len);
+    if (recv_len == -1) {
+        perror("서버 응답 수신 실패");
+        close(sock);
+        exit(1);
+    }
+
+    buffer[recv_len] = '\0';  // 응답 메시지 종료 문자 추가
+    printf("서버 응답: %s\n", buffer);
+
+    // 6. 소켓 종료
     close(sock);
-    printf("소켓 닫기 완료\n");
-
     return 0;
 }
--- Messenger/UDP/udp_server.c
+++ Messenger/UDP/udp_server.c
@@ -1,12 +1,60 @@
-//소켓 생성, 바인딩, 리슨, 클라이언트 연결, 데이터 송수신
-// socket(), bind(), listen(), accept(), send(), recv(), close()
-#include <netdb.h>
+// 소켓 생성, 바인딩, 리슨, 클라이언트 연결, 데이터 송수신
+//  socket(), bind(), listen(), accept(), send(), recv(), close()
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-int main(){
+#define PORT 9000
+#define BUF_SIZE 1024
+
+int main()
+{
     int sock;
+    struct sockaddr_in server_addr, client_addr;
+    char buffer[BUF_SIZE];
+    printf("buffer pointer : %p\n",buffer);
+    printf("buffer data : %d\n",&buffer);
+    printf("buffer data : %d\n",*buffer);
+    printf()
+    socklen_t addr_len = sizeof(client_addr);
+    // 1. UDP 소켓 생성
     sock = socket(AF_INET, SOCK_DGRAM, 0);
-    
+    if (sock == -1)
+    {
+        perror("소켓 생성 실패");
+        exit(1);
+    }
+    // 2. 서버 주소 구조체 설정
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    // 3. 소켓을 포트에 바인딩
+    if (bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
+        perror("바인딩 실패");
+        close(sock);
+        exit(1);
+    }
+    printf("UDP 서버 포트: %d\n", PORT);
+
+    // 4. 데잇터 수신 및 응답
+    while(1){
+        memset(buffer, 0, BUF_SIZE);
+        int recv_len = recvfrom(sock, buffer, BUF_SIZE, 0, (struct sockaddr*)&client_addr, &addr_len);
+        if(recv_len == -1){
+            perror("데이터 수신 실패");
+            break;
+        }
+        printf("Client:(%s:%d)\n",inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), buffer);
+        char response[BUF_SIZE];
+        sprintf(response, "UDP 서버 응답: %s",buffer);
+        if (sendto(sock, response, strlen(response), 0, (struct sockaddr*)&client_addr, addr_len) == -1) {
+            perror("응답 전송 실패");
+            break;
+        }        
+    }
 }
